c301910e36d7cb304519afab23d96b9ad115f9c2
Victor Volovik <VictorVolovik@users.noreply.github.com>
2025-01-10 13:49:12 +0400

    Add health check & refactor

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ const port = "8080"
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", handleHealthz)
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +21,10 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handleHealthz(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	res.Write([]byte("OK"))
 }
